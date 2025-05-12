1c93ea8d8c4bac65caaf0a6133013e138e441b1b
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-12 17:40:21 +0530

    Add Breadcrumb File

---
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,38 @@
+// src/components/Breadcrumbs.jsx
+import { Link, useLocation } from "react-router-dom";
+
+const Breadcrumbs = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
+  return (
+    <nav className="text-xl text-left text-gray-300 mb-6">
+      <ol className="flex items-center space-x-2">
+        <li>
+          <Link to="/" className="text-cyan-400 hover:underline">
+            Home
+          </Link>
+        </li>
+        {pathnames.map((value, index) => {
+          const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathnames.length - 1;
+
+          return (
+            <li key={to} className="flex items-center space-x-2">
+              <span className="mx-1">{">"}</span>
+              {isLast ? (
+                <span className="text-white capitalize">{value}</span>
+              ) : (
+                <Link to={to} className="text-cyan-400 hover:underline capitalize">
+                  {value}
+                </Link>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumbs;

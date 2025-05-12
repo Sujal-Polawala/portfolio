b7110f657f18d747d3fc8d843ca206c5bd13a809
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-12 17:48:51 +0530

    Add Projects

---
+++ src/data/projects.jsx
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    name: "TryNBuy",
+    url: "https://github.com/Sujal-Polawala/Major_Project",
+    description: "AI-based virtual shopping with image search, Stripe integration, and admin/seller portals.",
+  },
+  {
+    name: "Quote Generator",
+    url: "https://quote-generator-gold-six.vercel.app/",
+    description: "Generates random quotes from an API and allows sharing to Twitter.",
+  },
+  {
+    name: "SkyCast Weather",
+    url: "https://weather-app-frontend-taupe.vercel.app/",
+    description: "Weather forecasting app using real-time weather API for accurate location-based data.",
+  },
+];
+
+export default projects;

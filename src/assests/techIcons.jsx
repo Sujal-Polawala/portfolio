558501eda91e0ffc9c60de86af8fa0cb7cd1ada1
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-12 17:39:36 +0530

    Tech Icons and Name Added

---
+++ src/assests/techIcons.jsx
@@ -0,0 +1,31 @@
+const techIcons = [
+  {
+    name: "ReactJS",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    name: "NodeJS",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "ExpressJS",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  },
+  {
+    name: "MongoDB",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "HTML",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "SQL",
+    src: "https://www.svgrepo.com/show/331760/sql-database-generic.svg",
+  },
+];
+export default techIcons;

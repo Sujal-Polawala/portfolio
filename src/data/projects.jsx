bb00303170d6119200ad2f2f55daa113a4c72b4c
Sujal-Polawala <sujalpolawala@gmail.com>
2025-06-09 19:14:24 +0530

    Update Projects Data

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -3,6 +3,16 @@ const projects = [
     name: "TryNBuy",
     url: "https://github.com/Sujal-Polawala/Major_Project",
     description: "AI-based virtual shopping with image search, Stripe integration, and admin/seller portals.",
+    demos: {
+      user: "https://major-project-three-beta.vercel.app/",          // replace with actual user demo URL
+      admin: "https://trynbuy-admin.vercel.app", 
+    },
+    Credentials: {
+      admin:{
+        username: "admin123",
+        password: "admin1234",
+      }
+    }
   },
   {
     name: "Quote Generator",
